166037285fece8ded51a1f336f2ffe2760e1812a
OlesyaKhlybova <133571945+OlesyaKhlybova@users.noreply.github.com>
2024-05-08 19:30:07 +0300

    Update Main.java

--- src/main/java/ru/netology/service/Main.java
+++ src/main/java/ru/netology/service/Main.java
@@ -1,7 +1,9 @@
 package ru.netology.service;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
     }
 }
